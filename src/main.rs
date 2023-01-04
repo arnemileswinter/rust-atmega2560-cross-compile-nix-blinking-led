d69cd6d9ba0cc906b46e2faadf2cba8264910bb4
Arne Winter <arnemileswinter@gmail.com>
2023-01-04 15:16:31 +0100

    remove unused things, add print to the loop

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp,pins,57600);
 
     let mut led = pins.d13.into_output().downgrade();
 
     loop {
         led.toggle();
+        ufmt::uwriteln!(&mut serial, "hello world").unwrap();
         arduino_hal::delay_ms(1000);
     }
 }
